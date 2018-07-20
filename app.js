9a86a32265ab7b5320603ce10e6cb057b0128fc3
Ricardo Bos <ricardo@ricardobos.com>
2018-07-20 20:16:45 +0200

    Add Bootstrap

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
 app.get("/", function(req, res) {
-    res.send("Hello!");
+    res.render("index");
 });
 
 app.listen(3000);
